3f57693323f1a3d1e10715e02b9bcef881e8d006
lucas-tremaroli <lucasluigitremaroli@outlook.com>
2026-02-02 01:18:23 +0100

    feat(db): setup package and add db handler
    Signed-off-by: lucas-tremaroli <lucasluigitremaroli@outlook.com>

---
+++ datathon/db.py
@@ -0,0 +1,11 @@
+import duckdb
+from pandas import DataFrame
+
+
+class Database:
+    def __init__(self, db_path: str):
+        self.conn = duckdb.connect(database=db_path)
+
+    def fetch_table(self, table_name: str) -> DataFrame:
+        query = f"SELECT * FROM {table_name};"
+        return self.conn.execute(query).df()
